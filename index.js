35242cc82797cb3dd999e47a4544ff8b486daa6d
shashankjo <kumarshashank.jo@gmail.com>
2025-02-06 20:55:58 +0530

    Create a Redux action

---
+++ index.js
@@ -0,0 +1,14 @@
+const CAKE_ORDERED = 'CAKE_ORDERED'
+
+// an Action is an object that contains a type property, but it can contain more properties as well
+// Action creator is a function that that returns Action object
+
+function orderCake() {
+  return {
+    type: CAKE_ORDERED,
+    quantity: 10
+  }
+}
+
+console.log(orderCake())
+
