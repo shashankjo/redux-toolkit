a2838a361c32074503eb9e5e1b64ada79e6abb79
Kumar Shashank <40141542+shashankjo@users.noreply.github.com>
2025-02-06 21:39:45 +0530

    Create a redux store (#4)

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require('redux')
+const createStore = redux.createStore
+
 const CAKE_ORDERED = 'CAKE_ORDERED'
 
 // an Action is an object that contains a type property, but it can contain more properties as well
@@ -28,3 +31,25 @@ const reducer = (state = initialState, action) => {
       return state
   }
 }
+
+// Responsibilties of a redux store
+// 1. Holds application state, via a reducer
+const store = createStore(reducer)
+
+// 2. Allows access to state via getState()
+console.log('Initial state', store.getState())
+
+// 3. Registers listeners via subscribe(listener).
+// subscribe() is called automaticlly whenever the app state is updated by action dispatch
+const unsubscribe = store.subscribe(() =>
+  console.log('Updated state', store.getState())
+)
+
+// 4. Allows state to be updated via dispatch(action)
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+
+// 5. Handles unregistering of listeners via the function returned by subscribe(listener)
+unsubscribe()
+store.dispatch(orderCake()) // here subscribed() will not be invoked as we have unsubscribed from it above
